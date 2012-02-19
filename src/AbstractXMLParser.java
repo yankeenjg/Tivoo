10fe7fff4e6c916ffd756370c6d7f74b6e05ed59
James Hong <james.hong@duke.edu>
2012-02-19 18:40:28 -0500

    Modified abstract parser to comply with Template pattern

--- src/AbstractXMLParser.java
+++ src/AbstractXMLParser.java
@@ -1,6 +1,9 @@
 import java.util.*;
+
 import org.jdom.*;
 import org.jdom.input.*;
+import org.joda.time.DateTime;
+import org.joda.time.DateTimeZone;
 
 public abstract class AbstractXMLParser {
 	
@@ -23,8 +26,52 @@ public abstract class AbstractXMLParser {
 		return XMLParserList;
 	}
 		
-	public abstract List<Event> processEvents();
+	public List<Event> processEvents(){
+		
+		List<Element> xmlEventsList = parseGetEventsList();
+		List<Event> parsedEventsList = new ArrayList<Event>();
+		for(Element event : xmlEventsList){
+			String eventTitle 		= parseTitle(event);
+			String eventDescription = parseDescription(event);
+			String eventLocation 	= parseLocation(event);
+			DateTime startTime 		= parseStartTime(event);
+			DateTime endTime 		= parseEndTime(event);
+						
+			Event newEvent = new Event(eventTitle, startTime, endTime,
+										eventDescription, eventLocation);
+			parsedEventsList.add(newEvent);
+		}
+		
+		return parsedEventsList;
+	}
+	
+	public DateTime parseTime(Element time){
+		int year = parseYear(time);	
+		int month = parseMonth(time);	
+		int day = parseDay(time);	
+		
+		int hour24 = parseHour24(time);	
+		int minute = parseMinute(time);	
+		
+		DateTimeZone timeZone = parseTimeZone(time);
+		
+		return new DateTime(year, month, day, hour24, minute, timeZone);
+	}
+	
+	protected abstract List<Element> parseGetEventsList();
+	protected abstract DateTime parseStartTime(Element element);
+    protected abstract DateTime parseEndTime(Element element);
+    protected abstract String parseTitle(Element event);
+	protected abstract String parseDescription(Element event);
+	protected abstract String parseLocation(Element event);
 
+	protected abstract int parseYear(Element time);
+	protected abstract int parseMonth(Element time);
+	protected abstract int parseDay(Element time);
+	protected abstract int parseHour24(Element time);
+	protected abstract int parseMinute(Element time);
+	protected abstract DateTimeZone parseTimeZone(Element time);
+	
 	//implement this is subclasses
 	//public abstract boolean isThisType(String URL);
 	
